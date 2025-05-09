ef0aa4593a48e3731d46134675b0e2ea0d8dd621
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-05-09 14:31:40 +0200

    feat: inner 10 mails

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("ciao")
+
+const elemList = document.querySelector('.list-mail')
+
+
+let mail = ""
+for (let i = 0; i < 10; i++ ) {
+    axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((resp) => {
+            console.log(resp)
+            const elemMail = resp.data.response
+            mail += `<div class="list-mail">
+                        <ul>
+                            <li>${elemMail}</li>
+                        </ul>    
+                    </div>`
+
+            if (i = 9) {
+                elemList.innerHTML = mail
+            }           
+        })
+}
+
