cf92b511da4ad083c9a49bcbf70954dbdc351110
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-05-09 16:13:21 +0200

    feat: hidden initial 10 mails

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ const elemList = document.querySelector('.list-mail')
 const btnList = document.querySelector('#new-list-mail')
 
 
-let mail = ""
-for (let i = 0; i < 10; i++ ) {
-    axios
-        .get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then((resp) => {
-            console.log(resp)
-            const elemMail = resp.data.response
-            mail += `<li>${elemMail}</li>`   
-            elemList.innerHTML = mail         
-        })
-}
+// let mail = ""
+// for (let i = 0; i < 10; i++ ) {
+//     axios
+//         .get("https://flynn.boolean.careers/exercises/api/random/mail")
+//         .then((resp) => {
+//             console.log(resp)
+//             const elemMail = resp.data.response
+//             mail += `<li>${elemMail}</li>`   
+//             elemList.innerHTML = mail         
+//         })
+// }
 
 btnList.addEventListener("click", (event) => {
     event.preventDefault()
